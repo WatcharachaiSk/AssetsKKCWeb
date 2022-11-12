f68fc8cf25579269f3bb47432e602df26e357f42
WatcharachaiSk <watcharachai.sa@rmuti.ac.th>
2022-11-12 18:25:23 +0700

    Add

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+let name = "Eath";
+let age = 15;
+function App() {
+  return (
+    <div className="App">
+      Hello {name} + {age}
+    </div>
+  );
+}
+
+export default App;
